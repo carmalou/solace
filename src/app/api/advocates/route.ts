d6a4bb76c8ebc04cf4fee192015c375e49bdb6a7
Carmen Bourlon <carmenbourlonlong@gmail.com>
2025-08-10 23:02:48 -0500

    implement pagination

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,51 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { count } from "drizzle-orm";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+import { NextRequest } from "next/server";
 
-  const data = advocateData;
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const numPerPage = 10;
 
-  return Response.json({ data });
+  const page = searchParams.get("page");
+
+  console.log(
+    `
+    
+    page
+    
+    `,
+    page
+  );
+
+  const numericPage = parseInt(page || "0");
+
+  const offset = numericPage > 0 ? numericPage - 1 : 0;
+
+  const [{ count: total }] = await db
+    .select({ count: count() })
+    .from(advocates);
+
+  const numericTotal = Number(total);
+
+  const data = await db
+    .select()
+    .from(advocates)
+    .limit(numPerPage)
+    .offset(offset * numPerPage);
+
+  const paginatedResponse = {
+    data,
+    pagination: {
+      totalRecords: numericTotal,
+      totalPages: Math.ceil(numericTotal / numPerPage),
+      currentPage: numericPage,
+      perPage: numPerPage,
+      hasPrevPage: numericPage > 1,
+      hasNextPage: numericPage < Math.ceil(numericTotal / numPerPage),
+    },
+  };
+
+  return Response.json(paginatedResponse);
 }
